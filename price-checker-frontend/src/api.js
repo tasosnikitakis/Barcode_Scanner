045e52bdfe4448ffa4ac28dd68276b1ac0e0b4bc
tasos <tasosnikic@gmail.com>
2025-01-19 13:06:04 +0200

    Scanner Functionality pt 2

--- price-checker-frontend/src/api.js
+++ price-checker-frontend/src/api.js
@@ -1,7 +1,9 @@
 import axios from 'axios';
 
 const apiClient = axios.create({
-    baseURL: 'http://localhost:8000/api', // Backend base URL
+    baseURL: 'https://django-server-production-6689.up.railway.app/api/', // Backend base URL
+
+//    baseURL: 'http://localhost:8000/api', // Backend base URL
     headers: {
         'Content-Type': 'application/json',
     },
