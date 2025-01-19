ece75b304d8dba70d83f47832d340aca321b59de
tasos <tasosnikic@gmail.com>
2025-01-19 11:37:00 +0200

    Frontend - Backend sync pt 2

--- Barcode_Scanner/settings.py
+++ Barcode_Scanner/settings.py
@@ -35,6 +35,10 @@ CORS_ALLOW_ALL_ORIGINS = True
 # Comment out the following line and place your railway URL, and your production URL in the array.
 # CSRF_TRUSTED_ORIGINS = ["*"]
 
+CSRF_TRUSTED_ORIGINS = [
+    'https://django-server-production-6689.up.railway.app',  # Replace with your actual deployment domain
+]
+
 # Application definition
 
 INSTALLED_APPS = [
